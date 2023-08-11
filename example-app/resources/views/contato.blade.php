fc88a114cee141f3a56dfcf8925c00dacad8c05f
Ailton Sousa <ailton2017oliveira@gmail.com>
2023-08-11 00:20:12 -0300

    Esudo da framework de reposição

---
+++ example-app/resources/views/contato.blade.php
@@ -0,0 +1,13 @@
+<center>
+    <h1>Contatos</h1>
+
+    <p>Bara de contato teste apredendo laravel</p>
+
+    <a href="/">Voltar para home</a>
+
+    <button>
+         Ola lavarel
+    </button>
+
+
+</center>
